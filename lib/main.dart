0bbd42fa19737c407fee4693bea067af7a00ed4f
Arni-Rie Tamayo <arnirietamayo@gmail.com>
2026-04-20 12:31:07 +0800

    update

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  // runApp(MapApp());
+}
